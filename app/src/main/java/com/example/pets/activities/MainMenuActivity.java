30bc3024f25c7a7f715eab8a5936aca041140b00
Mohamed28 <mohamed28.lucas@gmail.com>
2020-05-24 23:28:42 -0300

    Main Menu fix

---
+++ app/src/main/java/com/example/pets/activities/MainMenuActivity.java
@@ -0,0 +1,28 @@
+package com.example.pets.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.pets.R;
+import com.example.pets.activities.clients.ClientsActivity;
+import com.example.pets.activities.users.UsersActivity;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu_activity);
+    }
+
+    public void goToUsers(View view) {
+        startActivity(new Intent(this, UsersActivity.class));
+    }
+
+    public void goToClients(View view) {
+        startActivity(new Intent(this, ClientsActivity.class));
+    }
+}
