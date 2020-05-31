4c7f54fe29cf304976760f5ded4c9ad11d73bf45
Mohamed28 <mohamed28.lucas@gmail.com>
2020-05-30 21:32:43 -0300

    Fix PetSeeds to avoid exceptions

--- app/src/main/java/com/example/pets/daos/seeds/PetsSeeds.java
+++ app/src/main/java/com/example/pets/daos/seeds/PetsSeeds.java
@@ -1,25 +1,36 @@
 package com.example.pets.daos.seeds;
 
+import android.widget.Toast;
+
+import com.example.pets.daos.ClientDAO;
 import com.example.pets.daos.PetDAO;
 import com.example.pets.models.Pet;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class PetsSeeds {
+    private static ClientDAO ownerDAO;
 
-     public static List<Pet> getCollection() {
+    public static List<Pet> getCollection() {
         List<Pet> pets = new ArrayList<>();
-        pets.add(new Pet("Ozzy", "Cachorro", "Pitbull", "Gabriel Camargo"));
-        pets.add(new Pet("Bob", "Cachorro", "Golden Retriever", "Lucas Barreto"));
-        pets.add(new Pet("Miau", "Gato", "Persian cat", "Nicholas"));
-        pets.add(new Pet("AuAu", "Cachorro", "Pastor Alemão", "Brenda"));
+        try {
+            pets.add(new Pet("Ozzy", "Cachorro", "Pitbull", ownerDAO.find(1)));
+            pets.add(new Pet("Bob", "Cachorro", "Golden Retriever", ownerDAO.find(1)));
+            pets.add(new Pet("Miau", "Gato", "Persian cat", ownerDAO.find(1)));
+            pets.add(new Pet("AuAu", "Cachorro", "Pastor Alemão", ownerDAO.find(1)));
+
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        }
         return pets;
     }
 
-    public static void install(PetDAO petDAO){
-        for (Pet pet : getCollection()){
+    public static void install(PetDAO petDAO, ClientDAO clientDAO) {
+        ownerDAO = clientDAO;
+        for (Pet pet : getCollection()) {
             petDAO.insert(pet);
         }
     }
